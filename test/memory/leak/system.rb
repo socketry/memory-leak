158690e647f870acb0aa16b45673a19c8f6dbd69
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2025-02-28 22:26:03 +1300

    Add more tests for `System` module.

--- test/memory/leak/system.rb
+++ test/memory/leak/system.rb
@@ -11,4 +11,36 @@ describe Memory::Leak::System do
 			expect(subject.total_memory_size).to be_a(Integer)
 		end
 	end
+	
+	with ".memory_usage" do
+		it "can determine the memory usage" do
+			expect(subject.memory_usage(Process.pid)).to be_a(Integer)
+		end
+		
+		it "returns 0 for invalid process ID" do
+			expect(subject.memory_usage(0)).to be == 0
+		end
+	end
+	
+	with ".memory_usages" do
+		it "can determine the memory usages" do
+			result = subject.memory_usages([Process.pid])
+			expect(result).to be_a(Array)
+			
+			result.each do |process_id, size|
+				expect(process_id).to be_a(Integer)
+				expect(size).to be_a(Integer)
+			end
+		end
+		
+		it "ignores invalid process IDs" do
+			result = subject.memory_usages([Process.pid, 0])
+			expect(result).to be_a(Array)
+			
+			result.each do |process_id, size|
+				expect(process_id).to be_a(Integer)
+				expect(size).to be > 0
+			end
+		end
+	end
 end
