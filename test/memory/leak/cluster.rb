97c96f34baf45bc33710bdf71ea23142207979cb
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2026-02-20 14:50:53 +1300

    Update copyrights.

--- test/memory/leak/cluster.rb
+++ test/memory/leak/cluster.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # Released under the MIT License.
-# Copyright, 2025, by Samuel Williams.
+# Copyright, 2025-2026, by Samuel Williams.
 
 require "memory/leak/cluster"
 require "memory/leak/a_leaking_process"
